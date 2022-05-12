672322d088dc0392f62f42a489789af612561eda
israe <israelalves45155b@gmail.com>
2022-05-12 15:39:41 -0300

    ajuste do footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,31 @@
+import { CompanyComponent } from './views/company/company.component';
 import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { AboutComponent } from './views/about/about.component'
+import { VideosComponent } from './views/videos/videos.component';
+import { ClientsComponent } from './views/clients/clients.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomeComponent
+  },
+  {
+    path: 'clientes',
+    component: ClientsComponent,
+  },
+  {
+    path: 'videos',
+    component: VideosComponent,
+  },
+  {
+    path: 'empresa',
+    component: CompanyComponent,
+  },
+  {
+    path: 'faleconosco',
+    component: AboutComponent,
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,28 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './views/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
-
+import { CompanyComponent } from './views/company/company.component';
+import { ClientsComponent } from './views/clients/clients.component';
+import { VideosComponent } from './views/videos/videos.component';
+import { AboutComponent } from './views/about/about.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    CompanyComponent,
+    ClientsComponent,
+    VideosComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
